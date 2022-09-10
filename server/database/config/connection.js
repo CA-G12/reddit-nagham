afde315996e1c8ad4db42a10df29434f5ffc7b04
naghamabuwarda <nasomabuwarda@gmail.com>
2022-09-10 21:01:03 +0300

    fixing relates #5

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -1,20 +1,20 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-let DATABASE_URL = '';
-if(process.env.NODE_ENV === 'development'){
-     DATABASE_URL = process.env.DEVELOPMENT_DATABASE;
-}else if(process.env.NODE_ENV === 'production'){
-    DATABASE_URL = process.env.PRODUCTION_DATABASE;
-}else if(process.env.NODE_ENV === 'testing'){
-    DATABASE_URL = process.env.TESTION_DATABASE
-}else{
-    console.log('NO DATABASE CONNECTION');
+let DATABASE_URL = "";
+if (process.env.NODE_ENV === "development") {
+  DATABASE_URL = process.env.DEVELOPMENT_DATABASE;
+} else if (process.env.NODE_ENV === "production") {
+  DATABASE_URL = process.env.DATABASE_URL;
+} else if (process.env.NODE_ENV === "testing") {
+  DATABASE_URL = process.env.TESTION_DATABASE;
+} else {
+  console.log("NO DATABASE CONNECTION");
 }
 
 const connection = new Pool({
     connectionString : DATABASE_URL,
-    ssl:process.env.NODE_ENV == 'production' ?{rejectUnauthorized: false}:false
+    ssl:process.env.NODE_ENV == 'production' ?{rejectUnauthorized: false}:falseو
 })
 
 module.exports = connection;
